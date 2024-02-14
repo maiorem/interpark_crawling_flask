d310fa15d93990284a7d95ca81e7d1a750d4f99e
Seyoung Hong <maiorem00@gmail.com>
2024-02-14 20:17:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import requests, sys, json
 import time
 
 from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
